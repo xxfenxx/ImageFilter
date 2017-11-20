af58b04fd6ff38067ea4cfb23f8d7c8be3c2e2cd
信勇哥得水牛 <517661395@qq.com>
2017-11-20 15:22:38 +0800

    1. 新增测试方法newMethod

--- ImageFilter/ImageFilter/AppDelegate.m
+++ ImageFilter/ImageFilter/AppDelegate.m
@@ -14,6 +14,9 @@
 
 @implementation AppDelegate
 
+- (void)newMethod {
+    
+}
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
